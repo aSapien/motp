469527974a02740ea1821ff42442c291dbd51535
Dima Ryskin <dimar@wix.com>
2017-08-06 17:34:34 +0300

    ensureFileSync for creating temp DB folder

--- mocha-setup.spec.js
+++ mocha-setup.spec.js
@@ -1,16 +1,23 @@
-const { writeFile } = require('fs-extra');
+const { writeFile, ensureFileSync } = require('fs-extra');
 require('chai').use(require('sinon-chai'));
 
 const log = require('debug')('motp:mocha-setup');
 
-const TEMP_STORGE_LOCATION = './temp/storage';
+const TEMP_STORGE_LOCATION = './temp/storage/keys.db';
+
+const initialSetup = (done) => {
+  log('Creating temp storage file...');
+  ensureFileSync(TEMP_STORGE_LOCATION);
+  done();
+};
 
 const setupAndTeardown = (done) => {
   log('Emptying embedded storage');
-  return writeFile(`${TEMP_STORGE_LOCATION}/keys.db`, '', 'utf-8', (err) => {
+  return writeFile(TEMP_STORGE_LOCATION, '', 'utf-8', (err) => {
     if (!err) return done();
     throw err;
   });
 };
 
+before(initialSetup);
 afterEach(setupAndTeardown);
